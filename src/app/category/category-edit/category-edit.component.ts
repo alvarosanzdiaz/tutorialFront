76744c2f7be8c3bcedcbf6ccfaac139446dd77d7
alvarosanzdiaz <alvarosanzdiaz@gmail.com>
2023-02-08 11:02:44 +0100

    Hasta nueva categoria

---
+++ src/app/category/category-edit/category-edit.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { CategoryService } from '../category.service';
+import { Category } from '../model/Category';
+
+@Component({
+  selector: 'app-category-edit',
+  templateUrl: './category-edit.component.html',
+  styleUrls: ['./category-edit.component.scss']
+})
+export class CategoryEditComponent implements OnInit {
+
+  category : Category;
+
+  constructor(
+    public dialogRef: MatDialogRef<CategoryEditComponent>,
+    private categoryService: CategoryService
+  ) { }
+
+  ngOnInit(): void {
+    this.category = new Category();
+  }
+
+  onSave() {
+    this.categoryService.saveCategory(this.category).subscribe(result => {
+      this.dialogRef.close();
+    });    
+  }  
+
+  onClose() {
+    this.dialogRef.close();
+  }
+
+}
---
+++ src/app/category/category-list/category-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Category } from '../model/Category';
+import { CategoryService } from '../category.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CategoryEditComponent } from '../category-edit/category-edit.component';
+
+@Component({
+  selector: 'app-category-list',
+  templateUrl: './category-list.component.html',
+  styleUrls: ['./category-list.component.scss']
+})
+export class CategoryListComponent implements OnInit {
+
+  dataSource = new MatTableDataSource<Category>();
+  displayedColumns: string[] = ['id', 'name', 'action'];
+
+  constructor(  private categoryService: CategoryService,public dialog: MatDialog,) { 
+  }
+
+  createCategory() {    
+    const dialogRef = this.dialog.open(CategoryEditComponent, {
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.ngOnInit();
+    });    
+  }  
+  
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(
+      categories => this.dataSource.data = categories
+    );
+  }
+
+}
