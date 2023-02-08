73a49f14955edfdaaf5c4934d16d96e718ad2558
alvarosanzdiaz <alvarosanzdiaz@gmail.com>
2023-02-08 13:06:02 +0100

    Eliminacion categorias

--- src/app/category/category-list/category-list.component.ts
+++ src/app/category/category-list/category-list.component.ts
@@ -4,6 +4,7 @@ import { Category } from '../model/Category';
 import { CategoryService } from '../category.service';
 import { MatDialog } from '@angular/material/dialog';
 import { CategoryEditComponent } from '../category-edit/category-edit.component';
+import { DialogConfirmationComponent } from 'src/app/core/dialog-confirmation/dialog-confirmation.component';
 
 @Component({
   selector: 'app-category-list',
@@ -18,6 +19,12 @@ export class CategoryListComponent implements OnInit {
   constructor(  private categoryService: CategoryService,public dialog: MatDialog,) { 
   }
 
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe(
+      categories => this.dataSource.data = categories
+    );
+  }
+
   createCategory() {    
     const dialogRef = this.dialog.open(CategoryEditComponent, {
       data: {}
@@ -27,11 +34,27 @@ export class CategoryListComponent implements OnInit {
       this.ngOnInit();
     });    
   }  
-  
-  ngOnInit(): void {
-    this.categoryService.getCategories().subscribe(
-      categories => this.dataSource.data = categories
-    );
-  }
+  deleteCategory(category: Category) {    
+    const dialogRef = this.dialog.open(DialogConfirmationComponent, {
+      data: { title: "Eliminar categoría", description: "Atención si borra la categoría se perderán sus datos.<br> ¿Desea eliminar la categoría?" }
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.categoryService.deleteCategory(category.id).subscribe(result => {
+          this.ngOnInit();
+        }); 
+      }
+    });
+  }  
+  editCategory(category: Category) {
+    const dialogRef = this.dialog.open(CategoryEditComponent, {
+      data: { category: category }
+    });
+    
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.ngOnInit();
+    });
+  }
 }
